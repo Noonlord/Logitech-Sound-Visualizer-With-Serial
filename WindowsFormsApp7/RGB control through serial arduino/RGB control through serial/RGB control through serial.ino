ea811b99154e0fa79dca956005d60fdd013b4adb
Noonlord <mymmym22@gmail.com>
2018-05-06 23:51:32 +0300

    Updated for new version of PC program

--- WindowsFormsApp7/RGB control through serial arduino/RGB control through serial/RGB control through serial.ino
+++ WindowsFormsApp7/RGB control through serial arduino/RGB control through serial/RGB control through serial.ino
@@ -1,23 +1,36 @@
 void setup() {
-  // put your setup code here, to run once:
-pinMode(9, OUTPUT);
-pinMode(6, OUTPUT);
-Serial.begin(57600);
+pinMode(9, OUTPUT); //RED 
+pinMode(6, OUTPUT); //BLUE
+pinMode(10, OUTPUT);//GREEN
+Serial.begin(115200);
 }
-
+int cts; //Color to set. If arduino read 101 before a color value it will set red brightness. 102 will make the same thing for green, 103 for red.
 void loop() {
-  // put your main code here, to run repeatedly:
-  int x, red, blue;
-  if (Serial.available() > 0) {
-    x = Serial.read();
-    if (x > 100){
-      red = map(x, 101, 201, 0, 255);
-      analogWrite(6, red);
-    } else {
-    if (x < 101){
-      blue = map(x, 0, 100, 0, 255);
-      analogWrite(9, blue);
-    }}
-    delay(2);
+  int x, red, blue, colorValue;
+  if (Serial.available() > 0) { //If serial port is available
+    x = Serial.read();          //Read serial port
+      if (x == 101){            //And if it is 101, 102 or 103 set cts. Which declares which color arduino will set when it reads a color value between 0 and 100.
+        cts = 101;
+      }
+      if (x == 102){
+        cts = 102;
+      }
+      if (x == 103){
+        cts = 103;
+      }
+      int colorValue = map(x, 0, 100, 0, 255); //We gave our color value between 0-100 but arduino can give values between 0 and 255. So we mapped our color value. 
+                                               //Actually if you want to lower the brightness you can lower the value 255
+      if (cts == 101){ //If cts is 101 and 
+        if (x < 101){  //Value coming from serial port is a color value
+        analogWrite(9, colorValue); //Set red brightness.
+        }}
+      if (cts == 102){ //Same for green
+        if (x < 101){
+        analogWrite(10, colorValue);
+        }}
+      if (cts == 103){ //Same for blue. 
+        if (x < 101){
+        analogWrite(6, colorValue);
+        }}
   }
 }
